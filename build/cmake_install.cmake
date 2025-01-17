8c2e02f5b179be43d2ec7227f39d1b73e7c35b8e
Nerinho Nascimento <neramornas@gmail.com>
2025-01-17 08:08:31 -0300

    remove: exclusão da implementação teclado.h
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Lucas/Desktop/Controle_GPIO_Teclado_Matricial_Subgrupo_3
+# Install script for directory: C:/Workspace/Embarcatech/PROJETO GPIO COM TECLADO MATRICIAL/Controle_GPIO_Teclado_Matricial_Subgrupo_3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Lucas/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Windows 10/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Lucas/Desktop/Controle_GPIO_Teclado_Matricial_Subgrupo_3/build/pico-sdk/cmake_install.cmake")
+  include("C:/Workspace/Embarcatech/PROJETO GPIO COM TECLADO MATRICIAL/Controle_GPIO_Teclado_Matricial_Subgrupo_3/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Lucas/Desktop/Controle_GPIO_Teclado_Matricial_Subgrupo_3/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Workspace/Embarcatech/PROJETO GPIO COM TECLADO MATRICIAL/Controle_GPIO_Teclado_Matricial_Subgrupo_3/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
